c54f33fd5675cc960055e9484296fd9a7f42f9b4
Taymegh <tmeghiche@gmail.com>
2023-12-10 23:04:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ import kivy
 
 a= 1
 
-from kivy.app import app
+from kivy.app import App
 from kivy.uix.label import Label
+
+
+class MyQuizz(App):
+    def build(self):
+        return Label(text="Enfin")
+    
