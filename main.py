643f4055c237b6326402faf83712059aa1b57700
subbu176 <153492494+subbu176@users.noreply.github.com>
2023-12-11 10:25:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from kivy.uix.label import Label
 class AppQuizz(App):
     
     def build(self):
-        return Label(text='Hello 3atay')
+        return Label(text='Hello everyone !!!')
     
 if __name__ == '__main__':
     AppQuizz().run()
